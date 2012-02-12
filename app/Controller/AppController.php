85188161905f3b721d167e7ee26a93ea06967090
John Wnek <john.wnek@gmail.com>
2012-02-11 23:26:52 -0600

    wompus

---
+++ app/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+// app/Controller/AppController.php
+class AppController extends Controller {
+    //...
+
+    public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
+
+    function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
+    //...
+}
